bc94a21c615f5dea112a8253e83e8d38ac3cd136
hhhhde <231250114@smail.nju.edu.cn>
2025-09-19 11:43:32 +0800

    Modified several "handle" methods to unify the way of receiving and processing response from backend

--- TomatoMall_Frontend/src/views/UserProfile.vue
+++ TomatoMall_Frontend/src/views/UserProfile.vue
@@ -4,16 +4,28 @@
       <template #header>
         <div class="card-header">
           <h2>个人信息</h2>
-          <el-button type="primary" @click="handleEdit" v-if="!isEditing">编辑</el-button>
+          <el-button type="primary" @click="handleEdit" v-if="!isEditing"
+            >编辑</el-button
+          >
         </div>
       </template>
-      
-      <el-form :model="userForm" :rules="rules" ref="userFormRef" label-width="100px" :disabled="!isEditing">
+
+      <el-form
+        :model="userForm"
+        :rules="rules"
+        ref="userFormRef"
+        label-width="100px"
+        :disabled="!isEditing"
+      >
         <el-form-item label="用户名" prop="username">
           <el-input v-model="userForm.username" disabled />
         </el-form-item>
         <el-form-item label="密码" prop="password" v-if="isEditing">
-          <el-input v-model="userForm.password" type="password" placeholder="不修改请留空" />
+          <el-input
+            v-model="userForm.password"
+            type="password"
+            placeholder="不修改请留空"
+          />
         </el-form-item>
         <el-form-item label="姓名" prop="name">
           <el-input v-model="userForm.name" />
@@ -33,13 +45,15 @@
         <el-form-item label="所在地" prop="location">
           <el-input v-model="userForm.location" />
         </el-form-item>
-        
+
         <el-form-item v-if="isEditing">
-          <el-button type="primary" @click="handleSave" :loading="loading">保存</el-button>
+          <el-button type="primary" @click="handleSave" :loading="loading"
+            >保存</el-button
+          >
           <el-button @click="handleCancel">取消</el-button>
         </el-form-item>
       </el-form>
-      
+
       <div class="logout-section">
         <el-button type="danger" @click="handleLogout">退出登录</el-button>
       </div>
@@ -48,115 +62,111 @@
 </template>
 
 <script setup>
-import { ref, reactive, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
-import { ElMessage } from 'element-plus'
-import { getUserInfo, updateUserInfo } from '../api/user'
+import { ref, reactive, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { ElMessage } from "element-plus";
+import { getUserInfo, updateUserInfo } from "../api/user";
 
-const router = useRouter()
-const userFormRef = ref(null)
-const loading = ref(false)
-const isEditing = ref(false)
+const router = useRouter();
+const userFormRef = ref(null);
+const loading = ref(false);
+const isEditing = ref(false);
 
 const userForm = reactive({
-  username: '',
-  password: '',
-  name: '',
-  role: '',
-  telephone: '',
-  email: '',
-  location: ''
-})
+  username: "",
+  password: "",
+  name: "",
+  role: "",
+  telephone: "",
+  email: "",
+  location: "",
+});
 
 const validatePhone = (rule, value, callback) => {
   if (value && !/^1[0-9]{10}$/.test(value)) {
-    callback(new Error('请输入正确的手机号'))
+    callback(new Error("请输入正确的手机号"));
   } else {
-    callback()
+    callback();
   }
-}
+};
 
 const validateEmail = (rule, value, callback) => {
   if (value && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
-    callback(new Error('请输入正确的邮箱地址'))
+    callback(new Error("请输入正确的邮箱地址"));
   } else {
-    callback()
+    callback();
   }
-}
+};
 
 const rules = {
-  name: [
-    { required: true, message: '请输入姓名', trigger: 'blur' }
-  ],
-  role: [
-    { required: true, message: '请选择角色', trigger: 'change' }
-  ],
-  telephone: [
-    { validator: validatePhone, trigger: 'blur' }
-  ],
-  email: [
-    { validator: validateEmail, trigger: 'blur' }
-  ]
-}
+  name: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+  role: [{ required: true, message: "请选择角色", trigger: "change" }],
+  telephone: [{ validator: validatePhone, trigger: "blur" }],
+  email: [{ validator: validateEmail, trigger: "blur" }],
+};
 
 const fetchUserInfo = async () => {
-  try {
-    const response = await getUserInfo(userForm.username)
-    if (response.data.code === '200') {
-      Object.assign(userForm, response.data.data)
-    }
-  } catch (error) {
-    ElMessage.error('获取用户信息失败')
-  }
-}
+  getUserInfo(userForm.username)
+    .then((res) => {
+      if (res.data.code === "200") {
+        Object.assign(userForm, res.data.data);
+      } else if (res.data.code === "400") {
+        ElMessage.error(res.data.msg || "获取用户信息失败");
+      }
+    });
+    // .catch((error) => {
+    //   ElMessage.error("获取用户信息失败，请稍后重试");
+    // });
+};
 
 const handleEdit = () => {
-  isEditing.value = true
-}
+  isEditing.value = true;
+};
 
 const handleCancel = () => {
-  isEditing.value = false
-  fetchUserInfo()
-}
+  isEditing.value = false;
+  fetchUserInfo();
+};
 
 const handleSave = async () => {
-  if (!userFormRef.value) return
-  
+  if (!userFormRef.value) return;
+
   await userFormRef.value.validate(async (valid) => {
     if (valid) {
-      loading.value = true
-      try {
-        const response = await updateUserInfo(userForm)
-        if (response.data.code === '200') {
-          ElMessage.success('更新成功')
-          isEditing.value = false
-        } else {
-          ElMessage.error(response.data.msg || '更新失败')
-        }
-      } catch (error) {
-        ElMessage.error('更新失败，请稍后重试')
-      } finally {
-        loading.value = false
-      }
+      loading.value = true;
+      updateUserInfo(userForm)
+        .then((res) => {
+          if (res.data.code === "200") {
+            ElMessage.success("更新成功");
+            isEditing.value = false;
+          } else if (res.data.code === "400") {
+            ElMessage.error(res.data.msg || "更新失败");
+          }
+          loading.value = false;
+        });
+        // .catch((error) => {
+        //   ElMessage.error("更新失败，请稍后重试");
+        //   loading.value = false;
+        // });
     }
-  })
-}
+  });
+};
 
 const handleLogout = () => {
-  localStorage.removeItem('token')
-  router.push('/login')
-}
+  localStorage.removeItem("token");
+  router.push("/login");
+};
 
 onMounted(() => {
   // 从localStorage获取用户名
-  const username = localStorage.getItem('username')
+  const username = localStorage.getItem("username");
   if (username) {
-    userForm.username = username
-    fetchUserInfo()
+    userForm.username = username;
+    fetchUserInfo();
   } else {
-    router.push('/login')
+    router.push("/login");
   }
-})
+});
 </script>
 
 <style scoped>
@@ -182,4 +192,4 @@ onMounted(() => {
   margin-top: 20px;
   text-align: center;
 }
-</style> 
+</style>
